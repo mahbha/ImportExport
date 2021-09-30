483081a8f47c7f68147658a6105c8ce87f33a79f
mahbha <43632182+mahbha@users.noreply.github.com>
2021-09-30 19:38:39 +0530

    import / export for multiple function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { add, subtract, multiply, divide } from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{add(1, 2)}</li>
+    <li>{subtract(5, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{divide(8, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
